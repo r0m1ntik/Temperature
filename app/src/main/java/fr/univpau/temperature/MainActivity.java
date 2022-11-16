d690fc2aa86a275b42982cbe00a5f7269f130b29
r0m1ntik <r.badanin94@yandex.ru>
2022-11-16 10:09:42 +0100

    Upd

--- app/src/main/java/fr/univpau/temperature/MainActivity.java
+++ app/src/main/java/fr/univpau/temperature/MainActivity.java
@@ -7,7 +7,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
